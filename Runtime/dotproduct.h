729f0ef962208cac60ec0e21a4e2d18346008c4b
k----n <kalvin@kalvineng.com>
2016-11-30 13:27:44 -0700

    dot product works!

--- Runtime/dotproduct.h
+++ Runtime/dotproduct.h
@@ -3,5 +3,59 @@
 #include "declarations.h"
 
 void dotProduct() {
+    _unwrap();
+    Value* value1 = stack->pop();
+    _unwrap();
+    Value* value2 = stack->pop();
 
+    if (!(value1)->isVector() || !(value2)->isVector()){
+        printf("Dot product must be of two vectors\n");
+        exit(1);
+    }
+
+    int size1 = value1->vectorValue()->getCount();
+    int size2 = value2->vectorValue()->getCount();
+
+    if (size1 != size2){
+        printf("Dot product must be of two vectors with the same length\n");
+        exit(1);
+    }
+
+    // only check one vector since the two vectors should have been promoted already
+    Value* node = value2->vectorValue()->get(0);
+    Value* node2;
+    if (node->isInteger()){
+        int total = 0;
+
+        for (int i = 0; i < size2; i++){
+            node = value2->vectorValue()->get(i);
+            node2 = value1->vectorValue()->get(i);
+
+            total += (*(node->integerValue()) * *(node2->integerValue()));
+        }
+
+        Value* dp = new Value(total);
+        stack -> push(dp);
+
+        return;
+    }
+    else if (node->isReal()){
+        float total = 0;
+
+        for (int i = 0; i < size2; i++){
+            node = value2->vectorValue()->get(i);
+            node2 = value1->vectorValue()->get(i);
+
+            total += (*(node->realValue()) * *(node2->realValue()));
+        }
+
+        Value* dp = new Value(total);
+        stack -> push(dp);
+
+        return;
+    }
+    else {
+          printf("Dot product must be of two vectors with numeric base types\n");
+          exit(1);
+    }
 }
